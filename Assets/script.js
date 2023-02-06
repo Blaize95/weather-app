7a80a22a02fd5a6858d746981ce90fae8f3de1cc
Blaize95 <118699442+Blaize95@users.noreply.github.com>
2023-02-06 11:28:28 +1000

    html structure and some stlying complete

---
+++ Assets/script.js
@@ -0,0 +1,11 @@
+//Variables
+var cityName = document.getElementById("search-input");
+var searchBtn = document.getElementById("search-btn");
+var searchedLocation = document.getElementById("searched-location");
+var resultCity = document.getElementById("city-name");
+var currentDay = document.getElementById("current-day");
+var weatherIcon = document.getElementById("weather-icon");
+var currentTemp = document.getElementById("temp");
+var currentWind = document.getElementById("wind");
+var currentHum = document.getElementById("hum");
+var forecast = document.getElementById("forecast");
